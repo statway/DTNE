da60ed55c1fa84e93cc791e1e9e7721a51eee378
statway <30316298+statway@users.noreply.github.com>
2024-09-22 11:04:08 +0800

    Add files via upload
    change docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '0.5'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.napoleon",
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
@@ -59,7 +60,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
